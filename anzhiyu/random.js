eefd80e16d3933112e6bb7f07e7d3f6aba4939e1
Aiohianeng <26317978080@qq.com>
2023-12-20 16:51:49 +0800

    Site updated: 2023-12-20 16:51:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["学习/mybatis代码生成器ui","学习/Centos7安装Docker","学习/docker总结"];function toRandomPost(){
+var posts=["Tools/mybatis代码生成器ui","学习/Centos7安装Docker","学习/docker总结"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
